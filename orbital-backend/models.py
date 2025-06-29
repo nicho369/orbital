24fe5e4bebfb337d44b738f779518df24f428dc0
stinkray <raychua0106@gmail.com>
2025-06-30 02:24:57 +0800

    changes

--- orbital-backend/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class StudyPlan(Base):
-    __tablename__ = "study_plans"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True)
-    json_data = Column(String)  # Store study plan JSON here
--- orbital-frontend/src/Message.tsx
+++
@@ -1,6 +0,0 @@
-// PascalCase for react
-function Message() {
-    return <h1>SoC GradWise</h1>
-}
-
-export default Message;
